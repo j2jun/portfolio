ce450bf53c103b8a1d6727cd6f1b8164c76b7f4b
Joseph Jun <54129361+j2jun@users.noreply.github.com>
2025-03-01 12:30:04 -0800

    Completed framer motion page transition.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,16 @@
+"use client";
+
 import Image from "next/image";
+import {motion} from "framer-motion";
 
 const Homepage = () => {
   return (
+    <motion.div 
+      className="h-full" 
+      initial={{y:"-200vh"}} 
+      animate={{y:"0%"}} 
+      transition={{ duration: 1 }}
+    >
     <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
       {/* IMAGE CONTAINER */}
       <div className="h-1/2 lg:h-full lg:w-1/2 relative">
@@ -10,7 +19,9 @@ const Homepage = () => {
       {/* TEXT CONTAINER */}
       <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
         {/* TITLE */}
-        <h1 className="text-4xl md:text-6xl font-bold">Crafting Digital Experiences, Designing Tomorrow.</h1>
+        <h1 className="text-4xl md:text-6xl font-bold">
+          Crafting Digital Experiences, Designing Tomorrow.
+        </h1>
         {/* DESC */}
         <p className="md:text-xl">
           Welcome to my digital canvas, where innovation and creativity 
@@ -25,6 +36,7 @@ const Homepage = () => {
         </div>
       </div>
     </div>
+    </motion.div>
   );
 };
 
